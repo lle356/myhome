97c9687369707cccf92c8ae64e941307f6579f01
이효성 <lle356@naver.com>
2022-04-05 11:55:15 +0900

    init

---
+++ src/test/java/com/hyosung/myhome/MyhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyosung.myhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
